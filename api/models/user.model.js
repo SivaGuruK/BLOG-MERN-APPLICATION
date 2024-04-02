4a6e83d27a63f225efb4f562925028bb08ed3094
SIVA <sivagurukarthickeyan@gmail.com>
2024-04-02 19:28:28 +0530

    Add OAuth Functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,21 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-    }
-}, {timestamps: true}
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://tse2.mm.bing.net/th?id=OIP.eCrcK2BiqwBGE1naWwK3UwHaHa&pid=Api&P=0&h=180",
+    },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema);
