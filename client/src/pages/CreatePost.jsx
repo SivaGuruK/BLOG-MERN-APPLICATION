06d56ecb5e9ada0ae919743a0d2c86a30e173823
SIVA <sivagurukarthickeyan@gmail.com>
2024-04-08 22:07:53 +0530

    COMPLETING SEARCH FUNCTIONALITY

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -104,9 +104,17 @@ export default function CreatePost() {
             }
           >
             <option value="uncategorized">Select a category</option>
+            <option value="html-css-js">HTML/CSS</option>
             <option value="javascript">JavaScript</option>
-            <option value="reactjs">React.js</option>
+            <option value="react">React</option>
+            <option value="angular">Angular</option>
             <option value="nextjs">Next.js</option>
+            <option value="vue">Vue.js</option>
+            <option value="nodejs">Node.js</option>
+            <option value="express">Express.js</option>
+            <option value="python">Python</option>
+            <option value="django">Django</option>
+            <option value="mongodb">MongoDB</option>
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
